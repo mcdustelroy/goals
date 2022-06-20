e551bdb8a3c5975cb9f6297ee7ff9e1ff0ce7b03
Dustin McElroy <dustinmcelroy2@hotmail.com>
2022-06-20 18:09:57 +0300

    added dotenv to server.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
+        // const conn = await mongoose.connect('mongodb+srv://dustin:dustin@cluster0.ayowv4h.mongodb.net/?retryWrites=true&w=majority')
 
         console.log(`mongoDB connected: ${conn.connection.host}`)
     } catch (err) {
